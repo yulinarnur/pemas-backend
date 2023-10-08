e1351d56ae6d6288824a84e492ef027eb245adec
YulinarNR <yulinarnurrahma@gmail.com>
2023-10-08 17:09:40 +0700

    menambahkan JWT Strategy

---
+++ src/authentication/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly prismaService: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrkey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { username: string }) {
+    const users = await this.prismaService.users.findUnique({
+      where: {
+        username: payload.username,
+      },
+    });
+    return users;
+  }
+}
