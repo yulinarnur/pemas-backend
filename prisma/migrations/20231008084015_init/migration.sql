472207cafeb34e4bc6bdd244f7110f15343d773e
YulinarNR <yulinarnurrahma@gmail.com>
2023-10-08 15:52:14 +0700

    prisma, docker dan users module

---
+++ prisma/migrations/20231008084015_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "nik" INTEGER NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "telp" VARCHAR(13) NOT NULL,
+    "username" VARCHAR(45) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("nik")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_username_key" ON "Users"("username");
